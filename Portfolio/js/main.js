c7b1bde66484c64022af6f06b37119326ffe0772
Paulo Cunha <37587744+PortugueseFullStackDeveloper@users.noreply.github.com>
2018-04-10 21:59:56 +0000

    Ecrã de loading adicionado e layout da homepage do portfólio praticamente completo

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,3 +1,25 @@
-// HOMEPAGE
+// LOADING SCREEN
+(function() {
 
-// Background aleatório da homepage
+		// Variáveis associadas à função do "preload"
+		var preload = document.getElementById('preload');
+		var loading = 0;
+		var id = setInterval(frame, 64);
+
+		// Função associada ao "loading"
+		function frame() {
+				if(loading == 100) {
+						clearInterval(id);
+
+						// Mudança para a página de index
+						window.open('index.html', '_self');
+				} else {
+						// Final da animação de loading (flash)
+						loading = loading + 1;
+						if(loading == 90) {
+							preload.style.animation = "fadeout 1s ease";
+						}
+				}
+		}
+
+})();
