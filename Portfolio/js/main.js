b164006f8ed16c31efb09876af91c25e496e2f02
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-04-16 01:03:09 +0000

    Sidemenu da página criado e dinamizado, ícones da secção dos temas melhorados, nome/logo da página ainda em 'hold'

---
+++ Portfolio/js/main.js
@@ -0,0 +1,11 @@
+/* MENU LATERAL */
+
+/* Função para abrir o menu lateral */
+function openSideMenu() {
+		document.getElementById('side-menu').style.width = '250px';
+}
+
+/* Função para fechar o menu lateral */
+function closeSideMenu() {
+		document.getElementById('side-menu').style.width = '0';
+}
